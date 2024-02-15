4f70c849468c53889c2a792e8900046c44b42916
ErfanMojibi <mojibierfan@gmail.com>
2024-02-15 03:59:10 +0330

    fix test

--- ClientPython/main.py
+++ ClientPython/main.py
@@ -1,25 +1,20 @@
 from client import pull, push, subscribe
-
+lst = []
+def on_subscribe(key, value):
+    global lst
+    print(f'recieved {key},{value} on subscribe')
+    lst.append((key,value))
+    
 def main():
-    print('[+] sending arshia1')
-    push('arshia', 'akhavan1')
-    print('[+] sending arshia2')
-    push('arshia2', 'akhavan2')
-    print('[+] sending arshia3')
-    push('arshia3', 'akhavan3')
-    print('[+] sending arshia4')
-    push('arshia4', 'akhavan4')
-    print('[+] sending arshia5')
-    push('arshia5', 'akhavan5')
-    print('[+] sending arshia6')
-    push('arshia6', 'akhavan6')
-    for i in range(6):
-        print(pull())
+    REMAINING = 30
+    for i in range(REMAINING):
+         key, val = pull()
+         print('pulled: ' , key, val)
 
     input('check kon okay bashe')
 
-    
-    KEY_RANGE = 100
+
+    KEY_RANGE = 30
     KEYS = 10
     push_seq = { _ : [] for _ in range(KEYS)}
     for i in range(KEY_RANGE):
@@ -30,24 +25,31 @@ def main():
     pull_seq = { _ : [] for _ in range(KEYS)}
     for i in range(KEY_RANGE):
         key, val = pull()
-        pull_seq[int(key)].append(val)
-        
+        pull_seq[int(key)].append(int(val))
+
+    for i in range(REMAINING):
+        key, val = pull()
+        print(key, val)
+
     print(f'push_seq', push_seq)
     print(f'pull_seq', pull_seq)
     input('is it ok?')
-    
-    
-    KEY_RANGE = 100
+
+
+    SUBSCRIBERS = 5
+    KEY_RANGE = 30
     KEYS = 10
-    for i in range(KEY_RANGE):
-        subscribe(print)
+    for i in range(SUBSCRIBERS):
+        subscribe(on_subscribe)
+        
     push_seq = { _ : [] for _ in range(KEYS)}
-    for i in range(KEY_RANGE):
+    for i in range(KEYS):
         print(f"[*] pushing {i} with key {i%KEYS}")
         push(f"{i%KEYS}", i)
-        push_seq[i].append(i)
-    
-    print('is subscribe working?')
+        push_seq[i%KEYS].append(i)
+    input('is subscribe working?')
+    print(lst)
+    input('how is it?')
     
 if __name__ == "__main__":
     main()
